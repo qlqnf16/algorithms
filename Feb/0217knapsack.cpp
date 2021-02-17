8a2e53b99a9f8aac187b2a471617beeb33b5f8a5
qlqnf16 <lee.jeongmin1027@likelion.org>
2021-02-17 20:56:31 +0900

    12865 평범한 배낭 DP

---
+++ Feb/0217knapsack.cpp
@@ -0,0 +1,24 @@
+﻿#include <cstdio>
+#define MAX 100000+10
+using namespace std;
+
+int N, K;
+int dp[MAX];
+
+int max(int a, int b) {
+	return a > b ? a : b;
+}
+
+int main() {
+	scanf("%d %d", &N, &K);
+	for (int i = 0; i < N; i++) {
+		int w, v;
+		scanf("%d %d", &w, &v);
+
+		for (int k = K; k >= w; k--) {
+			dp[k] = max(dp[k], dp[k - w] + v);
+		}
+	}
+	printf("%d", dp[K]);
+	return 0;
+}
