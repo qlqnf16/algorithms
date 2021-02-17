36033b44ef759b475cedabf393f0aa5c6a330a23
qlqnf16 <lee.jeongmin1027@likelion.org>
2021-02-17 15:55:09 +0900

    15353큰수(2)

---
+++ Feb/0217bigInteger.cpp
@@ -0,0 +1,37 @@
+﻿#include <cstdio>
+#include <algorithm>
+#include <string>
+#include <iostream>
+#define MAX 10010
+using namespace std;
+	
+string x, y;
+string result;
+
+void sum() {
+	int xl = x.length();
+	int num, carry = 0;
+	for (int i = 0; i < xl; i++) {
+		int tempSum = (x[i] - '0') + (y[i] - '0') + carry;
+		num = tempSum % 10;
+		carry = tempSum / 10;
+		result += num + '0';
+	}
+	if (carry) result += carry + '0';
+}
+
+void solve() {
+	reverse(x.begin(), x.end());
+	reverse(y.begin(), y.end());
+	while (x.length() < y.length()) x += '0';
+	while (x.length() > y.length()) y += '0';
+	sum();
+	reverse(result.begin(), result.end());
+}
+
+int main() {
+	cin >> x >> y;
+	solve();
+	printf("%s", result.c_str());
+	return 0;
+}
