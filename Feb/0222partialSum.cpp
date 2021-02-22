3c58adbf76b40f5b627bb1ba8adb80bb6bd2ed30
qlqnf16 <lee.jeongmin1027@likelion.org>
2021-02-22 22:41:42 +0900

    1806 부분합 / 투포인터

---
+++ Feb/0222partialSum.cpp
@@ -0,0 +1,41 @@
+﻿#include <cstdio>
+#include <algorithm>
+#define MAX 100010
+using namespace std;
+
+int N, S;
+int nums[MAX];
+
+int solve() {
+	int tempSum = nums[0];
+	int minNums = MAX;
+	int si = 0;
+	int ei = 0;
+	bool flag = false;
+	while (ei < N && si < N) {
+		if (tempSum < S) {
+			ei++;
+			tempSum += nums[ei];
+		}
+		else {
+			flag = true;
+			minNums = min(minNums, ei - si + 1);
+			if (minNums == 1) break;
+			tempSum -= nums[si];
+			si++;
+		}
+	}
+
+	return flag ? minNums : 0;
+}
+
+int main() {
+	scanf("%d %d", &N, &S);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &nums[i]);
+	}
+	int ans = solve();
+	printf("%d", ans);
+
+	return 0;
+}
